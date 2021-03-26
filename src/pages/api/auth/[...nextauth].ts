dc0d863f06803ed1694599ddc0d8e77ed3b6cda4
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-03-26 12:05:22 -0300

    Persisting Users in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb';
+
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,38 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-})
+  callbacks: {
+    async signIn(user, acount, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  },
+});
