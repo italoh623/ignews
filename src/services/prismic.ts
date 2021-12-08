fd49fbb55e62c36fb07d3e4816da4dad3659ab44
Ítalo Henrique <italohenrique014@gmail.com>
2021-12-08 11:22:59 -0300

    Update local variable name

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,7 +2,7 @@ import Prismic from '@prismicio/client';
 
 export function getPrismicClient(req?: unknown) {
   const prismic = Prismic.client(
-    process.env.PRISMIC_ENDPOINT,
+    process.env.PRISMIC_API_ENDPOINT,
     {
       req,
       accessToken: process.env.PRISMIC_ACCESS_TOKEN
